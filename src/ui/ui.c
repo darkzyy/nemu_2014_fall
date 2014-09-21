26baa18b30dc1208ab25cb3c049405ceea5a582e
131220070-Zhou Yaoyang <diamondzyy@sina.com>
2014-09-21 13:35:13 +0800

    now make"si x" come true

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -80,14 +80,32 @@ restart_:
 	cmd_c();
 }
 
-static void cmd_si(){
+static void cmd_si(unsigned int a){
 	if(nemu_state == END){
 		restart();
 		nemu_state =STOP;
 		nemu_state = RUNNING;
 		}
-	cpu_exec(1);
+	cpu_exec(a);
 }
+
+static void cmd_info_reg(){
+	if(nemu_state == END) {
+        puts("The Program does not start. Use 'r' command to start the program.");
+        return;
+	}
+	printf("eax            0x%x            %d\n",cpu.eax,cpu.eax);
+	printf("ecx            0x%x            %d\n",cpu.ecx,cpu.ecx);
+	printf("edx            0x%x            %d\n",cpu.edx,cpu.edx);
+	printf("ebx            0x%x            %d\n",cpu.ebx,cpu.ebx);
+	printf("esp            0x%x            %d\n",cpu.esp,cpu.esp);
+	printf("ebp            0x%x            %d\n",cpu.ebp,cpu.ebp);
+	printf("esi            0x%x            %d\n",cpu.esi,cpu.esi);
+	printf("edi            0x%x            %d\n",cpu.edi,cpu.edi);
+	printf("eip            0x%x            %d\n",cpu.eip,cpu.eip);
+
+}
+
 
 void main_loop() {
 	char *cmd;
@@ -100,7 +118,16 @@ void main_loop() {
 		if(strcmp(p, "c") == 0) { cmd_c(); }
 		else if(strcmp(p, "r") == 0) { cmd_r(); }
 		else if(strcmp(p, "q") == 0) { return; }
-		else if(strcmp(p,"si") == 0) { cmd_si(); }
+		else if(strcmp(p,"si") == 0) {
+			p =strtok(NULL," ");
+			if(p!=NULL){
+		   		unsigned int a=0;
+				sscanf(p,"%u",&a);
+				cmd_si(a);
+			}
+			else cmd_si(1);
+	   	}
+		else if(strcmp(p,"infor") == 0) { cmd_info_reg(); }
 
 		/* TODO: Add more commands */
 
